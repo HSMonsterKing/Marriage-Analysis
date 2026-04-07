a7f9b4b4eebc051a8417f7248d7814cf154116ab
HSMonsterKing <stanleyoreo@yahoo.com.tw>
2026-04-07 11:25:43 +0800

    20260407

--- .vscode/Web_Crawler.py
+++ .vscode/Web_Crawler.py
@@ -10,7 +10,25 @@ def url_response(data):
         f.write(r.content)
 
 urls = [
-("2018_01_10140-01-02-2_臺中市 結婚人數按性別、年齡、原屬國籍(地區)及婚前婚姻狀況分暨其結婚年齡之第一四分位數、中位數、第三四分位數與平均數(按發生日期)", "https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=28e43506-087a-4912-8d9b-aba999757f1c")
+("2018_01_10140-01-02-2_臺中市 結婚人數按性別、年齡、原屬國籍(地區)及婚前婚姻狀況分暨其結婚年齡之第一四分位數、中位數、第三四分位數與平均數(按發生日期).JSON", "https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=28e43506-087a-4912-8d9b-aba999757f1c")
+,("2018_01_10140-01-03-2_臺中市 結婚人數按性別、單一年齡、五歲年齡及初婚、再婚分 (按發生日期).JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=5707ec45-12bd-4587-ba04-20eddf3e46c0")
+,("2018_01_10140-01-04-2_臺中市 結婚對數按男女雙方年齡分(按發生日期).JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=83bddfbe-17c0-4c9e-b7ac-ee50f4a6427a")
+,("2018_01_10140-01-05-2_臺中市 初婚人數按性別、年齡及教育程度分暨其結婚年齡中位數(按發生日期).JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=d6fc32fe-5cde-4247-93ff-145cf2441e05")
+,("2018_01_10140-03-01-2_臺中市各區 離婚人數按離婚者性別及年齡分 (按發生日期).JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=c46cc29b-d7b1-4f53-8319-fc68dc0bcf27")
+,("2018_01_10140-03-02-2_臺中市各區 離婚對數按男方年齡及女方原屬國籍(地區)與年齡分(按發生日期）.JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=18cd3b39-34ec-4a09-9f6b-e346e61698ba")
+,("2018_01_10140-03-03-2_臺中市各區 離婚對數按男方教育程度及女方原屬國籍(地區)與教育程度分(按發生日期).JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=22f4c41b-1988-4345-bfb5-37c344521a19")
+,("2018_10140-03-08-2 臺中市各區 離婚人數按離婚者性別及年齡分 (按登記日期).JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=f5b2db9e-69f6-4cb8-aa1b-5d87fd8fdc4a")
+,("2018_01_30220-01-04-2_臺中市山地平地原住民婚姻狀況.JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=a5e6b01e-ab21-4df3-8162-d392113cb219")
+,("2018_10_10140-03-21-2_臺中市各區 離婚人數按離婚者男女雙方原屬國籍(地區)分.JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=b3c5d2df-0c37-410e-9108-577e7671a0ab")
+,("2018_09_10140-03-21-2_臺中市各區 離婚人數按離婚者男女雙方原屬國籍(地區)分.JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=ac474b5d-be13-499c-9bdb-1c0928fdcbd3")
+,("2018_08_10140-03-21-2_臺中市各區 離婚人數按離婚者男女雙方原屬國籍(地區)分.JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=716597d3-3be5-46f4-b37d-adc938cfa219")
+,("2018_07_10140-03-21-2_臺中市各區 離婚人數按離婚者男女雙方原屬國籍(地區)分.JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=ad2823c9-2109-449b-b908-25783dc7242a")
+,("2018_06_10140-03-21-2_臺中市各區 離婚人數按離婚者男女雙方原屬國籍(地區)分.JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=d778f3f6-57eb-416e-8736-b0b8a2cb46fc")
+,("2018_05_10140-03-21-2_臺中市各區 離婚人數按離婚者男女雙方原屬國籍(地區)分.JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=28da7db1-2521-4e77-97ad-bb26589a560d")
+,("2018_04_10140-03-21-2_臺中市各區 離婚人數按離婚者男女雙方原屬國籍(地區)分.JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=2e02fb34-2bd2-4a7f-bda9-6fb16337bb97")
+,("2018_03_10140-03-21-2_臺中市各區 離婚人數按離婚者男女雙方原屬國籍(地區)分.JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=737af98d-912e-4d67-94b3-11f5162312cd")
+,("2018_02_10140-03-21-2_臺中市各區 離婚人數按離婚者男女雙方原屬國籍(地區)分.JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=f236f7ea-8213-455a-aa9f-529a025dc709")
+,("2018_01_10140-03-21-2_臺中市各區 離婚人數按離婚者男女雙方原屬國籍(地區)分.JSON","https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=220a4a8f-bc05-4393-9f0c-6efd611c809f")
 ]
 
 start = time()
---
+++ .vscode/Web_Crawler_BANK_OF_TAIWAN.py
@@ -0,0 +1,92 @@
+import tkinter as tk
+from tkinter import scrolledtext as st
+import requests
+from bs4 import BeautifulSoup
+from tkinter import messagebox as msg
+import pyodbc
+
+
+ww = tk.Tk()
+ww.title("我的Python視窗")
+ww.geometry("800x600")
+
+def ConnectSQL(dbName,userName,pwd):
+    srv = "marriage-analysis.database.windows.net"
+    sqlConn = ("DRIVER={ODBC Driver 17 for SQL Server};SERVER=" +
+               srv + ";DATABASE=" + dbName + ";UID=" + userName + ";PWD=" + pwd)
+    try:
+        coxn = pyodbc.connect(sqlConn)
+        return coxn
+    except Exception as e:
+        msg.showerror("失敗", str(e))
+def Btn4_Click():
+    myurl="https://rate.bot.com.tw/xrt/quote/l6m/USD"
+    ans=""
+    result=requests.get(myurl)
+    result.encoding="utf-8"
+    bs=BeautifulSoup(result.text,"html.parser")
+
+    ans1=bs.find_all("td",{"class":"text-center"})
+    for zz in ans1:
+        ans += zz.text + "\n"
+
+    ans2 = bs.find_all("td", {"class": "rate-content-sight text-right print_table-cell"})
+    for zz in ans2:
+        ans += zz.text + "\n"
+
+    St1.delete("1.0","end")
+    St1.insert("1.0",ans)
+
+def Btn5_Click():
+    myurl="https://rate.bot.com.tw/xrt/quote/l6m/USD"
+    ans=""
+    result=requests.get(myurl)
+    result.encoding="utf-8"
+    bs=BeautifulSoup(result.text,"html.parser")
+
+    ans1=bs.find_all("td",{"class":"text-center"})
+    lst1=[]
+    for zz in ans1:
+        lst1.append(zz.text)
+    lst2=[]
+    for i in range (0,len(lst1),2):
+        lst2.append(lst1[i])
+    del lst1
+
+    lst3 = []
+    ans2 = bs.find_all("td", {"class": "rate-content-sight text-right print_table-cell"})
+    for zz in ans2:
+        lst3.append(zz.text)
+
+    final_lst=[]
+    for i in range (len(lst2)):
+        ll=[]
+        ll.append(lst2[i])
+        ll.append(lst3[i])
+        ll.append(lst3[i+1])
+        final_lst.append(ll)
+
+    print(final_lst)
+
+    St1.delete("1.0","end")
+    St1.insert("1.0",final_lst)
+    cnn = ConnectSQL("Marriage-Analysis", "stanleyoreo", "Zxcv1234")
+    tsql = "INSERT INTO [美金匯率] VALUES(?,?,?)"
+    for i in range(len(final_lst)):
+        cnn.execute(tsql, (str(final_lst[i][0]),float(final_lst[i][1]),float(final_lst[i][2])))
+        cnn.commit()  # 認可交易行為
+    cnn.close()
+    La1["text"] = "新增資料成功"
+    msg.showinfo("成功", "新增資料成功")
+
+La1=tk.Label(ww,text="請輸入：",bg="yellow",width=10)
+La1.place(x=10,y=10)
+Btn4=tk.Button(ww,text="按鈕4",width=10,height=1,command=Btn4_Click)
+Btn5=tk.Button(ww,text="按鈕5",width=10,height=1,command=Btn5_Click)
+St1=st.ScrolledText(ww,wrap=tk.WORD)
+Btn4.place(x=200,y=100)
+Btn5.place(x=300,y=100)
+St1.place(x=10,y=200)
+
+
+ww.mainloop()
